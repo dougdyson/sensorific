cc23750e27492522bb10c0ca74dbd964388a164d
Unfamous Dyson <dougdyson@gmail.com>
2020-11-30 13:37:01 -0800

    add simplified sensor component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 
-import sensors from './data/sensors.js';
+import sensorsData from './data/sensors.js';
 import readings from './data/readings.js';
 
-import Sensor from './components/Sensor/Sensor' //?
+import Sensor from './Components/Sensor/Sensor' //?
+import { useState } from 'react';
 
-console.log('App.js sensors:', sensors);
-console.log('App.js readings:', readings);
+// console.log('App.js readings:', readings);
 
-const device = sensors[0].name;
-const reading = readings[2].value.toFixed(1);
+// const reading = readings[2].value.toFixed(1);
 
 function App() {
+  
+  const devices = sensorsData;
+  console.log('App.js sensors:', devices);
+  
+  const [sensors, setSensors] = useState([devices]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Sensor />
-        <p>{reading}°C</p>
+        <ul>
+          { devices.map(device => (
+            <Sensor name={device.name} type={device.type} />
+        ))}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Sensor/Sensor.jsx
+++ src/components/Sensor/Sensor.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import sensors from '../../data/sensors.json';
-import List from '@material-ui/core/List'
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
 export default function Sensor(props) {
   
-  const sensor = sensors.map(device => {return device.name});
-  console.log('sensors:', sensor);
+  //sensors.map(device => {return device.name});
+  console.log('sensors props:', props);
+
+  const device = props.name;
+  const type = props.type;
 
   return (
     <div>
-        <li>
-          {sensor}
-        </li>
+      <li>
+        {device} - {type}
+      </li>
     </div>
   );
 }
