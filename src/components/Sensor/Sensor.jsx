e572df7764b595f47286d877def547a5327f5732
Unfamous Dyson <dougdyson@gmail.com>
2020-11-30 04:09:26 -0800

    add list element

--- src/components/Sensor/Sensor.jsx
+++ src/components/Sensor/Sensor.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import sensors from '../../data/sensors.json';
+import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem';
 
 export default function Sensor(props) {
   
-  const sensor = sensors[0].name;
+  const sensor = sensors.map(device => {return device.name});
   console.log('sensors:', sensor);
 
   return (
     <div>
-      <ListItem>{sensor}</ListItem>
+        <li>
+          {sensor}
+        </li>
     </div>
   );
 }
