7a17d779f0cae32332bad2ae2c97e3b975ddce09
Unfamous Dyson <dougdyson@gmail.com>
2020-12-01 07:50:27 -0800

    add last readings to dashboard

--- src/Components/Sensor/SensorsList.jsx
+++ src/Components/Sensor/SensorsList.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-// import readings from '../data/readings.json';
+import readings from '../../data/readings';
 // import List from '@material-ui/core/List';
 // import ListItem from '@material-ui/core/ListItem';
 
 export default function Sensor(props) {
   
   const device = props.devices.type.split(' ');
-  const type = device[0]; 
-  // const sensorID = props.devices.id;
+  const type = device[0];
 
+  function getLatestSensorReadingById (readings, id) {
+    // assumes data is sorted chronologically
+    const sensorReadings = readings.filter((reading) => (
+      id === reading.sensorId
+    ));
+    const lastReading = sensorReadings.pop();
+    if (lastReading) {
+      return lastReading;
+    } else {
+      return '--';
+    }
+  }
+
+  const latestReading = getLatestSensorReadingById(readings, props.devices.id);
+  
+  let reading = parseFloat(latestReading.value).toFixed(1);
+
+  if (isNaN(reading)) {
+    reading = '--';
+  }
+  
   return (
     <div>
-      {type}
+      <p>{reading} {props.devices.units}<br></br>{type}</p>
     </div>
   );
 }
